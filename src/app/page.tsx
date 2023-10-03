d0faf79dc124c0f83fe4c659da701a4807808f10
Rahul-jpeg <rahulveer262@gmail.com>
2023-10-04 01:09:17 +0530

    hero section fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Talk to your <span className="text-blue-600">documents</span> in
-          seconds.
+          Chat with your <span className="text-blue-600">documents</span>{' '}
+          easily.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
           Celit allows you to chat with your PDFs like it&apos;s your friend.
