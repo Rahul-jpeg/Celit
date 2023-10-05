e280adc6292382cc7c808651b04b3dd4d9c14a5c
Rahul-jpeg <rahulveer262@gmail.com>
2023-10-05 11:32:50 +0530

    added dashboard page

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,9 +1,27 @@
 'use client';
 
 import { useState } from 'react';
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
+import { Button } from './ui/button';
 
 const UploadButton = () => {
-  const [isOpen, setIsOpen] = useState();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger asChild onClick={() => setIsOpen(true)}>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+      <DialogContent>Upload here</DialogContent>
+    </Dialog>
+  );
 };
 
 export default UploadButton;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,11 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { publicProcedure, router } from './trpc';
+import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import { z } from 'zod';
 
 export const appRouter = router({
+    // authCallback procedure
     authCallback: publicProcedure.query(async () => {
         const { getUser } = getKindeServerSession()
         const user = getUser()
@@ -27,6 +29,36 @@ export const appRouter = router({
         }
 
         return { success: true };
+    }),
+    // procedure to get the files
+    getUserFiles: privateProcedure.query(async ({ ctx }) => {
+        const { userId, user } = ctx
+
+        return await db.file.findMany({
+            where: {
+                userId
+            }
+        })
+    }),
+    // procedure to delete a file
+    deleteFile: privateProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        const { userId } = ctx;
+
+        const file = await db.file.findFirst({
+            where: {
+                id: input.id,
+                userId,
+            },
+        })
+
+        if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+        await db.file.delete({
+            where: {
+                id: input.id,
+            },
+        })
+        return file;
     })
 })
 
