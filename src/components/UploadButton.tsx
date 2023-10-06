938af58b7c87a9a7603985da498da1c5eb7a1d30
Rahul-jpeg <rahulveer262@gmail.com>
2023-10-06 12:25:12 +0530

    file upload dropzone and file page layout

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,61 @@
 import { useState } from 'react';
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 import { Button } from './ui/button';
+import Dropzone from 'react-dropzone';
+import { Cloud, FileIcon } from 'lucide-react';
+import { Progress } from './ui/progress';
+
+const UploadDropzone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => console.log(acceptedFile)}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-gray-500">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500">PDF (up to 4MB)</p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-content-center">
+                    <FileIcon className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress value={50} className="h-1 w-full bg-zinc-200" />
+                </div>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -19,7 +74,9 @@ const UploadButton = () => {
       <DialogTrigger asChild onClick={() => setIsOpen(true)}>
         <Button>Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>Upload here</DialogContent>
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
